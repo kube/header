cb417915eaec693af5243fd02d87db74d1ae55bc
Chris Feijoo <chris@kube.io>
2020-03-22 13:59:01 +0100

    Remove embedded type def and move Jest config to dedicated file

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+
+      /*#######.
+     ########",#:
+   #########',##".
+  ##'##'## .##',##.
+   ## ## ## # ##",#.
+    ## ## ## ## ##'
+     ## ## ## :##
+      ## ## ##*/
+
+module.exports = {
+  preset: 'ts-jest/presets/default',
+  moduleFileExtensions: ['js', 'ts'],
+  testMatch: ['**/*.spec.ts']
+}
